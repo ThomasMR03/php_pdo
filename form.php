49d42f9280ff206f50dc37ccbcd34ebfd0f971a5
Thomas <Thomas.Face03@gmail.com>
2017-03-15 09:49:43 +0100

    Formulaire

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulaire client</title>
+</head>
+<body>
+<h1>Ajout de client</h1>
+
+<form method="post" action="">
+	<input type="text" name="nom" placeholder="Nom">
+	<input type="text" name="prenom" placeholder="Prénom">
+	<input type="date" name="naissance">
+	<label for="card">Le client a t'il une carte de fidélité ?</label>
+	<input type="checkbox" name="card">
+	<input type="number" name="numeCard" placeholder="Numéro de la carte">
+</form>
+</body>
+</html>
