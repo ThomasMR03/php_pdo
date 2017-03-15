66d81854f17c6651e6077d3bb2e80b90abdf7674
Thomas <Thomas.Face03@gmail.com>
2017-03-15 10:33:47 +0100

    Form update

--- form.php
+++ form.php
@@ -1,3 +1,41 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=colyseum;charsert=utf8', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+?>
+
+<?php
+ if (isset($_POST) $$ !empty($_POST)) {
+ 	$erreur = [];
+ 	
+ 	if (isset($_POST['nom']) && $_POST['nom']==''){
+ 		$nom = $_POST['nom'];
+ 	}else{
+ 		$erreur[] = "Merci de mettre un nom";
+ 	}
+ 	if (isset($_POST['prenom']) && $_POST['prenom']=='') {
+ 		$nom = $_POST['prenom'];
+ 	}else{
+ 		$erreur[] = "Merci de mettre un prénom";
+ 	}
+ 	if (isset($_POST['naissance'])) {
+ 		$nom = $_POST['naissance'];
+ 	}else{
+ 		$erreur[] = "Merci de mettre votre date de naissance";
+ 	}
+ 	if (isset($_POST['card'])) {
+ 		$card = 1;
+ 		if (isset($_POST['numeCard'])) {
+ 			$numeCard = $_POST['numeCard'];
+ 		}else{
+ 			$erreur[] = "Merci de mettre un numéro de carte";
+ 		}
+ 	}else{
+ 		$card = 0;
+ 	}
+ 	echo "Post effecuté !";
+ }
+?>
 <!DOCTYPE html>
 <html>
 <head>
